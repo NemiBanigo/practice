0db79346abe1ba8b5bc2fb788c5105de4db57467
Nemi <nemibanigo@gmail.com>
2017-04-09 18:44:04 +0100

    add remaining files

---
+++ fizzbuzz-test.js
@@ -0,0 +1,50 @@
+function fizzBuzz(x) {
+  if(x % 5 === 0 && x % 3 === 0){
+    return "FizzBuzz";
+  } else if (x % 5 === 0){
+    return "Buzz";
+  } else if (x % 3 === 0) {
+    return "Fizz";
+  } else if (x % 5 !== 0 && x % 3 !== 0){
+    return x ;
+  }
+}
+
+
+
+
+describe("Fizz Buzz tests ", function() {
+
+  it("should return `Fizz` for number divisible by 3", function() {
+    expect(fizzBuzz(3)).toBe('Fizz');
+  });
+
+  it("should return `Buzz` for number divisible by 5", function() {
+    expect(fizzBuzz(5)).toBe('Buzz');
+  });
+
+  it("should return `FizzBuzz` for 15", function() {
+    expect(fizzBuzz(15)).toBe('FizzBuzz');
+  });
+
+  it("should return `FizzBuzz` for 45", function() {
+    expect(fizzBuzz(45)).toBe('FizzBuzz');
+  });
+
+  it("should return `FizzBuzz` for 90", function() {
+    expect(fizzBuzz(90)).toBe('FizzBuzz');
+  });
+
+  it("should return `Fizz` for 63", function() {
+    expect(fizzBuzz(63)).toBe('Fizz');
+  });
+
+  it("should return 7 since its indivisible by 3 and 5", function() {
+    expect(fizzBuzz(7)).toBe(7);
+  });
+
+  it("should return 101 since its indivisible by 3 and 5", function() {
+    expect(fizzBuzz(101)).toBe(101);
+  });
+
+});
